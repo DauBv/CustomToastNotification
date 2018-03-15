41fdd0bd5a72fcbf0facd658c389cfef9fd2a2f5
daubv <developer.daubv@gmail.com>
2018-03-15 15:09:52 +0700

    commit project custom toast notificaion

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # CustomToastNotification
+
+![alt tag](http://sv1.upsieutoc.com/2017/07/24/Screenshotfrom2017-07-2414-28-59.png)
+![alt tag](http://sv1.upsieutoc.com/2017/07/24/Screenshotfrom2017-07-2414-29-13.png)
+![alt tag](http://sv1.upsieutoc.com/2017/07/24/Screenshotfrom2017-07-2414-29-32.png)
+
+
+
+
---
+++ app/src/main/java/demo/test/com/customtoastnotification/MainActivity.java
@@ -0,0 +1,43 @@
+package demo.test.com.customtoastnotification;
+
+import android.databinding.DataBindingUtil;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Gravity;
+import android.widget.Toast;
+
+import java.util.Random;
+
+import demo.test.com.customtoastnotification.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        ActivityMainBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        binding.setListener(this);
+    }
+
+    public void onShowToast() {
+        CustomToastNotification customToastNotification = new CustomToastNotification(this);
+        Random random = new Random();
+        int rd = random.nextInt(3);
+        switch (rd) {
+            case 0:
+                customToastNotification.setIcon(R.mipmap.ic_launcher);
+                break;
+            case 1:
+                customToastNotification.setIcon(R.drawable.minion).setTitle("Minion");
+                break;
+            case 2:
+                customToastNotification.setIcon(R.mipmap.ic_launcher_round).setTitle("Round");
+                break;
+        }
+        customToastNotification.setMessage("Type : " + rd);
+        Toast toast = Toast.makeText(this, "a", Toast.LENGTH_LONG);
+        toast.setGravity(Gravity.BOTTOM | Gravity.FILL_HORIZONTAL, 0, 0);
+        toast.setView(customToastNotification);
+        toast.show();
+    }
+}
